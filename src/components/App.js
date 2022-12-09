dad4310a17a8c0e4a2a072489ff9d05bce0fbb3c
rafaelaboliver <rafaelaa.oliveiraa.rb@gmail.com>
2022-12-09 09:22:00 -0300

    projeto criado com todos os arquivos necessários do template, npm do axios/router/styled-components instalados e globalstyle feito.

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+export default function App() {
+    return (
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route  />
+            <Route  />
+            <Route  />
+          </Routes>
+        </BrowserRouter>
+      )
+ 
+}
